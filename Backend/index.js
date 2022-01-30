7085b3c634a8b1fdcbf59b532b076b6516b0de11
Saikat Mukherjee <75084468+saikatmu31@users.noreply.github.com>
2022-01-31 00:25:36 +0530

    Adding Another Route

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,9 @@ const PORT = 4000
 app.get("/", (req, res) => {
     return res.json({ "Message": "Hello" })
 })
+app.get("/home", (req, res) => {
+    return res.send("Home Route")
+})
 
 app.listen(PORT, () => {
     console.log("Server is Up and Running");
