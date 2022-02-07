c92fba33ff6f4ab60a9d91df8fd35439294c5684
Debdeep1 <deb.baner.1@gmail.com>
2022-02-07 11:34:46 +0530

    OTP page

---
+++ Frontend/src/AEC-OTP/src/components/Check.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Check() {
+  return <button className="checkOTP">Check OTP</button>;
+}
+
+export default Check;
---
+++ Frontend/src/AEC-OTP/src/components/OTP.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Buttons from "./Buttons.jsx";
+import Check from "./Check.jsx";
+function OTP() {
+  return (
+    <div className="OTPSection">
+      <h2>Verify Your OTP</h2>
+      <Buttons />
+      <Buttons />
+      <Buttons />
+      <Buttons />
+      <Buttons />
+    </div>
+  );
+}
+
+export default OTP;
