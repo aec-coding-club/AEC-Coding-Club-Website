7571c4be60258c4e530b97a3886d05203adc6cdd
Soumya Banerjee <soumyabanerjee783@gmail.com>
2022-02-07 19:21:43 +0530

    Added Members Page, Removed Navbar Curve

--- Frontend/src/Components/MemberCard.jsx
+++ Frontend/src/Components/MemberCard.jsx
@@ -1,7 +1,68 @@
-import React from "react";
+import React from 'react'
+import { FaGithubAlt, FaLinkedin } from 'react-icons/fa'
 
-const MemberCard = () => {
-  return <div>Member Card</div>;
-};
+import DefaultImg from '../Assets/members/member.png'
+import './styles/MemberCard.css'
 
-export default MemberCard;
+const MemberCard = ({ member }) => {
+  const { img, name, linkedin, github } = member
+  return (
+    <div className='member-card'>
+      <div className='card-img-container'>
+        <img
+          src={img ? img : DefaultImg}
+          alt='profile-image'
+          className='card-img'
+        />
+      </div>
+      <h3 className='card-name'>{name}</h3>
+      {(linkedin || github) && (
+        <div className='card-links'>
+          {linkedin && (
+            <a
+              href={linkedin}
+              className='card-link'
+              target='_blank'
+              rel='noopener'
+            >
+              <FaLinkedin />
+            </a>
+          )}
+          {github && (
+            <a
+              href={github}
+              className='card-link'
+              target='_blank'
+              rel='noopener'
+            >
+              <FaGithubAlt />
+            </a>
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default MemberCard
+
+{
+  /* <img
+        src={img ? img : DefaultImg}
+        alt='profile-image'
+        className='card-img'
+      />
+      <h3 className='card-name'>{name}</h3>
+      <div className='card-links'>
+        {linkedin && (
+          <a href={linkedin} target='_blank' rel='noopener'>
+            <FaLinkedin />
+          </a>
+        )}
+        {github && (
+          <a href={github} target='_blank' rel='noopener'>
+            <FaGithubAlt />
+          </a>
+        )}
+      </div> */
+}
