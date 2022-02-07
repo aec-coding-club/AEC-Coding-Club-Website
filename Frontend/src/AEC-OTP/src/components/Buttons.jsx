6f7152184ddf9f5d14dea2b1b5ee60af5f4df45b
Pranay <pranayguptamain@gmail.com>
2022-02-07 11:41:49 +0530

    Revert "OTP page"
    This reverts commit c92fba33ff6f4ab60a9d91df8fd35439294c5684.

--- Frontend/src/AEC-OTP/src/components/Buttons.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Buttons() {
-  return <input className="InputBox" maxLength="1"></input>;
-}
-
-export default Buttons;
--- Frontend/src/AEC-OTP/src/components/Check.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Check() {
-  return <button className="checkOTP">Check OTP</button>;
-}
-
-export default Check;
