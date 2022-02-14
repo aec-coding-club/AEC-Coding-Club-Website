093da9190afe05c5a09e243723981c3566e4f6ec
Abir Pal <abirpal282002@gmail.com>
2022-02-14 19:48:15 +0530

    conditional rendering

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
+import axios from "axios";
 import Home from './Pages/Home'
 import Events from './Pages/Events'
 import Members from './Pages/Members'
@@ -10,10 +10,33 @@ import Otpverify from './Pages/OTP'
 import Dashboard from './Pages/Dashboard-Secure'
 import Errorpage from './Pages/Errorpage'
 import './App.css'
+import {Api} from './backend'
+import UserContext from './Pages/Context/LoggedUserContext'
 
 const App = () => {
+  const [userdata, setUserdata] = useState({})
+
+  async function fetchdata() {
+    const parseddata = await axios.get(`${Api}dashboard`, {
+      withCredentials: true,
+    });
+    // if(!parseddata.data.token){
+    //   console.log("Navigating");
+    //   navigate("/");
+    // }
+    console.log(parseddata);
+    setUserdata({
+      userInfo : parseddata.data.user_data
+    })
+  }
+
+  useEffect(() => {
+    fetchdata();
+  }, []);
+
   return (
     <>
+    <UserContext.Provider value={userdata}>
       <Router>
         <Routes>
           <Route exact path='/' element={<Home />} />
@@ -26,6 +49,7 @@ const App = () => {
           <Route exact path="*" element={<Errorpage />} />
         </Routes>
       </Router>
+      </UserContext.Provider>
     </>
   )
 }
--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
 import Navbar from '../Components/Navbar'
+import UserContext from './Context/LoggedUserContext'
 import './styles/Home.css'
 
 const Home = () => {
+  const details = useContext(UserContext)
+  console.log("details", details);
   return (
     <>
-      <Navbar />
+      <Navbar userImage={details.userInfo.pimage} userNameText={details.userInfo.name}/>
       <main className="home-main">
         <h1>Home Page</h1>
       </main>
