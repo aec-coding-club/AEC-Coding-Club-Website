c55b18520c4e57c839d160d7aa8af52447b90526
Abir Pal <abirpal282002@gmail.com>
2022-02-19 18:49:37 +0530

    Add events section fixed

--- Frontend/src/Pages/Events.jsx
+++ Frontend/src/Pages/Events.jsx
@@ -1,51 +1,60 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { FaEdit } from 'react-icons/fa'
-import './styles/Events.css'
-import Navbar from '../Components/Navbar'
-import EventModal from '../Components/EventModal'
-import EventsContainer from '../Components/EventsContainer'
-import UserContext from './Context/LoggedUserContext'
+import React, { useContext, useEffect, useState } from "react";
+import { FaEdit } from "react-icons/fa";
+import "./styles/Events.css";
+import Navbar from "../Components/Navbar";
+import EventModal from "../Components/EventModal";
+import EventsContainer from "../Components/EventsContainer";
+import UserContext from "./Context/LoggedUserContext";
 
 const Events = ({ tokenChecker }) => {
-  const [modalShow, setModalShow] = React.useState(false)
+  const [modalShow, setModalShow] = React.useState(false);
 
   function onHide() {
-    setModalShow(false)
+    setModalShow(false);
   }
 
   return (
     <>
       <main className='events-main'>
         {tokenChecker && (
-          <div className='events-header'>
-            <div className='events-header-left'>
-              <p>Hi, {tokenChecker[1]}</p>
+          <div>
+            <div className='events-header'>
+              <div className='events-header-left'>
+                <p>Hi, {tokenChecker[1]}</p>
+              </div>
+              <div className='events-header-right'>
+                <img
+                  src='../Assets/events/events-header.jpg'
+                  alt='events'
+                  className='events-header-img'
+                />
+              </div>
             </div>
-            <div className='events-header-right'>
-              <img
-                src='../Assets/events/events-header.jpg'
-                alt='events'
-                className='events-header-img'
+            <div className='events-section'>
+              <div className='header-wrapper'>
+                <h3 className='events-section-header'>Upcoming Events</h3>
+                <button
+                  className='event-btn'
+                  onClick={() => setModalShow(true)}
+                >
+                  <div>Add Event</div>{" "}
+                  <div>
+                    <FaEdit />
+                  </div>
+                </button>
+              </div>
+              <EventModal
+                addEvent={true}
+                modalShow={modalShow}
+                onHide={onHide}
               />
             </div>
           </div>
         )}
-        <div className='events-section'>
-          <div className='header-wrapper'>
-            <h3 className='events-section-header'>Upcoming Events</h3>
-            <button className='event-btn' onClick={() => setModalShow(true)}>
-              <div>Add Event</div>{' '}
-              <div>
-                <FaEdit />
-              </div>
-            </button>
-          </div>
-          <EventModal addEvent={true} modalShow={modalShow} onHide={onHide} />
-          <EventsContainer />
-        </div>
+        <EventsContainer />
       </main>
     </>
-  )
-}
+  );
+};
 
-export default Events
+export default Events;
