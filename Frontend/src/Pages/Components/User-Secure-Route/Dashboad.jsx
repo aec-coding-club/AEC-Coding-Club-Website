d2c55486d764cb9c642a73c38aaa872d707dfd90
Abir Pal <abirpal282002@gmail.com>
2022-02-26 10:05:37 +0530

    Dashboard is Up and Running

--- Frontend/src/Pages/Components/User-Secure-Route/Dashboad.jsx
+++ Frontend/src/Pages/Components/User-Secure-Route/Dashboad.jsx
@@ -1,55 +1,78 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { Api } from '../../../backend'
-import PreviewEventCard from '../../../Components/PreviewEventCard'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Api } from "../../../backend";
+import PreviewEventCard from "../../../Components/PreviewEventCard";
+import "../../../Components/styles/EventsContainer.css";
 
 const DashboadComponent = ({ details }) => {
-  const [events, setEvents] = useState([])
+  const [events, setEvents] = useState([]);
+  const [userRole, setUserRole] = useState(0);
 
   const fetchEvents = async () => {
     const allEvents = details.userInfo.events.map(async (id) => {
       const response = await axios.get(`${Api}${id}`, {
         withCredentials: true,
-        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
-      })
-      return response.data
-    })
-    setEvents(await Promise.all(allEvents))
-  }
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      });
+      return response.data;
+    });
+    setEvents(await Promise.all(allEvents));
+
+    // TODO: Set the Role of the user
+    const authToken = localStorage.getItem("token");
+    const parseddata = await axios.get(`${Api}dashboard`, {
+      withCredentials: true,
+      headers: { Authorization: `Bearer ${authToken}` },
+    });
+    setUserRole(parseddata.data.user_data.role);
+  };
 
   useEffect(() => {
-    fetchEvents()
-  }, [])
+    fetchEvents();
+  }, []);
 
   return (
     <>
       <div className='home-main'>
-        {console.log('Events Array: ', events && events)}
-        <h1>You are Verified and Authorized Welcome to Dashboard Route</h1>
+        {console.log("Events Array: ", events && events)}
+
         <br />
         <br />
       </div>
-      <div>
-        {events.length > 0 ? (
-          events.map((event) => (
-            <PreviewEventCard
-              key={event._id}
-              cardEditData={{
-                editEventTitle: event.eventTitle,
-                editEventImage: event.eventImage,
-                editEventTime: event.eventTime,
-                editEventDetails: event.eventDetails,
-                dashboardEvents: true,
-              }}
-            />
-          ))
-        ) : (
-          <h3>You have not registered to any events</h3>
-        )}
-      </div>
+      <main className='events-main'>
+        <div className='events-container'>
+          {events.length > 0 && userRole <= 2 ? (
+            events.reverse().map((event) => (
+              <>
+                <PreviewEventCard
+                  key={event._id}
+                  cardEditData={{
+                    editEventTitle: event.eventTitle,
+                    editEventImage: event.eventImage,
+                    editEventTime: event.eventTime,
+                    editEventDetails: event.eventDetails,
+                    dashboardEvents: true,
+                  }}
+                />
+              </>
+            ))
+          ) : (
+            <>
+              {userRole >= 3 ? (
+                <>
+                  <h2>Welcome to the Admin Panel</h2>
+                  {/* TODO: Admin Panel Here */}
+                </>
+              ) : (
+                <h2>You Have Not Registered To Any Events</h2>
+              )}
+            </>
+          )}
+        </div>
+      </main>
       <br />
     </>
-  )
-}
+  );
+};
 
-export default DashboadComponent
+export default DashboadComponent;
