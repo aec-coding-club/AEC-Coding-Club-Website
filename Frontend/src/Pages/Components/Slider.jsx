d99672d327fb9c70542c8c8e4d8479987a989a24
soumali28 <gorai.soumali@gmail.com>
2022-03-02 20:15:17 +0530

    Slider

--- Frontend/src/Pages/Components/Slider.jsx
+++ Frontend/src/Pages/Components/Slider.jsx
@@ -13,11 +13,13 @@ const Slider = () => {
   const nextSlide = () => {
     setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
     console.log("next");
+   
   };
 
   const prevSlide = () => {
     setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
     console.log("prev");
+
   };
 
   function auto() {
@@ -34,6 +36,7 @@ const Slider = () => {
     }
     return () => clearInterval(slideInterval);
   }, [currentSlide]);
+
 
   return (
     <div className='slider'>
