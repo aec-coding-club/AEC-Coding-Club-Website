dd6849935b836886fd47c6e1e559c1690043bc60
Saikat Mukherjee <75084468+saikatmu31@users.noreply.github.com>
2022-03-09 16:46:42 +0530

    Admin Pannel ON PROGRESS

--- Backend/routes/alllogs.js
+++ Backend/routes/alllogs.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const getlogs = require("../controllers/alllogs");
+const { getlogs, branchdata,yeardata } = require("../controllers/alllogs");
 const {
   isAuthenticated,
   isActivated,
@@ -8,5 +8,7 @@ const {
 } = require("../middlewares/verify");
 
 router.get("/logger", isAuthenticated, isActivated, isAdmin, getlogs);
+router.get("/branchdata", isAuthenticated, isActivated, isAdmin, branchdata);
+router.get("/yeardata", isAuthenticated, isActivated, isAdmin, yeardata);
 
 module.exports = router;
