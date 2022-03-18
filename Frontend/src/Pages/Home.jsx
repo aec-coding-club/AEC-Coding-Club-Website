126d2bcc6f66cf588ebf70bee103fdb2fdb8181a
Soumali Gorai <66300439+soumali28@users.noreply.github.com>
2022-03-18 12:12:11 +0530

    intro and objective added (#1)
    Co-authored-by: Aritra Biswas <93366359+Aritra777@users.noreply.github.com>
    Co-authored-by: Abir Pal <74393266+Abirpal202049@users.noreply.github.com>

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -6,16 +6,25 @@ import UserContext from "./Context/LoggedUserContext";
 import "./styles/Home.css";
 
 import HomeCard from "./Components/HomeCard";
-import Winners from "../data/homeCard";
+import {Winners, IntroCardsData} from "../data/homeCard";
 import Slider from "./Components/Slider";
 import Logo from "../Assets/logo.svg";
 
 import Numbers from "../Components/Numbers";
+
+import {IntroCards} from './Components/IntroCards';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 import TypedComponent from "./Components/Typed";
 
 
 const Home = () => {
   const [show, setShow] = useState(true);
+  useEffect(()=>{
+    AOS.init({duration:1000});
+  }, [])
 
   const date = new Date();
 
@@ -32,8 +41,13 @@ const Home = () => {
           </div>
         </div>
 
+
+//         <div className='content'>
+          {/* <button
+
         <div className="content">
           <button
+
             onClick={() => setShow(!show)}
             id="btntogglediv"
             className="btn"
@@ -61,7 +75,43 @@ const Home = () => {
                 </p>
               </div>
             </section>
-          ) : null}
+          ) : null} */}
+
+          <div className="sections">
+            <p className="section-heading"  data-aos="fade-up">
+              WHO ARE WE ?
+              <hr id="inro-head-hr"/>
+            </p>
+            <div className="intro-cards">
+              {IntroCardsData.map((items,index)=>{
+                return(
+                  <IntroCards key={index} {...items}/>
+                )
+              })}
+            </div>
+          </div>
+          <div className="sections" data-aos="fade-up">
+            <p className="section-heading">
+              Objectives
+              <hr id="inro-head-hr"/>
+            </p>
+            <div className="section-text">
+              <p className="section-text-body">
+              The AEC Coding Club started its journey on 11 April 2021. Its
+                  major objective is to motivate students' interest in learning
+                  various technologies and to obtain information about how
+                  technology will be used to project work. The club's goal is to
+                  encourage students to solve various challenging problems using
+                  demanding technologies which include Python, Java, C, C++,
+                  etc. Our approach will help students to become Industry Ready.
+                  This will boost them to get Job. The AEC Coding Club actively
+                  organizes various activities like Online Coding Contest,
+                  Online Quiz Contest, etc. Other events like Expert's Talk,
+                  Alumni Talk, and Hands-on practical workshops for various
+                  programming basics are planned for the future.
+              </p>
+            </div>
+          </div>
 
           {/* <div className='winners'>
             <h2 className='winners-heading'>Last Event Winners</h2>
