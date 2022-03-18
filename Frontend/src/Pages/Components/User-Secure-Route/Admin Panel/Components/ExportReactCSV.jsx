b6b41032a8d2fe84a3b9be600e8cfb2dc92e80c1
Soumya Banerjee <soumyabanerjee783@gmail.com>
2022-03-18 18:11:48 +0530

    Export Participant Data To .xlsx !!!

---
+++ Frontend/src/Pages/Components/User-Secure-Route/Admin Panel/Components/ExportReactCSV.jsx
@@ -0,0 +1,22 @@
+import * as FileSaver from 'file-saver'
+import * as XLSX from 'xlsx'
+
+export const ExportReactCSV = ({ csvData, fileName }) => {
+  const fileType =
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8'
+  const fileExtension = '.xlsx'
+
+  const exportToCSV = (csvData, fileName) => {
+    const ws = XLSX.utils.json_to_sheet(csvData)
+    const wb = { Sheets: { data: ws }, SheetNames: ['data'] }
+    const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' })
+    const data = new Blob([excelBuffer], { type: fileType })
+    FileSaver.saveAs(data, fileName + fileExtension)
+  }
+
+  return (
+    <button className='btn' onClick={(e) => exportToCSV(csvData, fileName)}>
+      Export
+    </button>
+  )
+}
--- Frontend/src/Pages/Components/User-Secure-Route/Admin Panel/Pages/AdminEvents.jsx
+++ Frontend/src/Pages/Components/User-Secure-Route/Admin Panel/Pages/AdminEvents.jsx
@@ -1,32 +1,32 @@
-import React, { useEffect, useState } from "react";
-import { Api } from "../../../../../backend";
-import axios from "axios";
+import React, { useEffect, useState } from 'react'
+import { Api } from '../../../../../backend'
+import axios from 'axios'
 import { v4 as uuidv4 } from 'uuid'
 import EventBlock from '../Components/EventBlock'
 import '../Styles/admin-event.css'
 
 const AdminEvents = () => {
-  const [eventData, seteventdata] = useState([]);
-  const [displayData, setDisplayData] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [eventData, seteventdata] = useState([])
+  const [displayData, setDisplayData] = useState([])
+  const [loading, setLoading] = useState(false)
 
   const fetchEvents = async () => {
-    setLoading(true);
-    const authToken = localStorage.getItem("token");
+    setLoading(true)
+    const authToken = localStorage.getItem('token')
 
     const { data } = await axios.get(`${Api}eventsdata`, {
       withCredentials: true,
       headers: { Authorization: `Bearer ${authToken}` },
-    });
-    console.log("Event data ", data.eventdata);
-    seteventdata(await data.eventdata);
-    setDisplayData(data.eventdata);
-    setLoading(false);
-  };
+    })
+    console.log('Event data ', data.eventdata)
+    seteventdata(await data.eventdata)
+    setDisplayData(data.eventdata)
+    setLoading(false)
+  }
 
   useEffect(() => {
-    fetchEvents();
-  }, []);
+    fetchEvents()
+  }, [])
 
   return (
     <>
@@ -35,12 +35,14 @@ const AdminEvents = () => {
           {loading ? (
             <p>Loading...</p>
           ) : (
-            displayData.map((event) => <EventBlock key={uuidv4()} eventdetails={event} />)
+            displayData.map((event) => (
+              <EventBlock key={uuidv4()} eventdetails={event} />
+            ))
           )}
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default AdminEvents;
+export default AdminEvents
