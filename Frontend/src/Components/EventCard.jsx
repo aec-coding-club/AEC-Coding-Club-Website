4d15e1e318834bdca8ae9c5d634a035664b16232
Soumya Banerjee <soumyabanerjee783@gmail.com>
2022-03-24 00:00:16 +0530

    Register button removed from previous event

--- Frontend/src/Components/EventCard.jsx
+++ Frontend/src/Components/EventCard.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import "./styles/EventCard.css";
-import ConfirmModal from "./ConfirmModal";
-import { Link, useNavigate } from "react-router-dom";
-import { Api } from "../backend";
-import axios from "axios";
-import { useState } from "react";
-import { toast } from "react-toastify";
-import ReactMarkdown from "react-markdown";
-import moment from "moment";
+import React from 'react'
+import './styles/EventCard.css'
+import ConfirmModal from './ConfirmModal'
+import { Link, useNavigate } from 'react-router-dom'
+import { Api } from '../backend'
+import axios from 'axios'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
+import ReactMarkdown from 'react-markdown'
+import moment from 'moment'
 
 export const EventCard = ({
   cardData,
@@ -19,13 +19,13 @@ export const EventCard = ({
   decisionArray,
   name,
 }) => {
-  const { eventImage, eventTitle, eventDetails, eventTime, _id } = cardData;
+  const { eventImage, eventTitle, eventDetails, eventTime, _id } = cardData
   // console.log("Card data :- ", cardData);
-  let navigate = useNavigate();
+  let navigate = useNavigate()
 
-  const [showConfirm, setShowConfirm] = useState(false);
-  const [showNotifyConfirm, setShowNotifyConfirm] = useState(false);
-  const [confirmId, setConfirmId] = useState("");
+  const [showConfirm, setShowConfirm] = useState(false)
+  const [showNotifyConfirm, setShowNotifyConfirm] = useState(false)
+  const [confirmId, setConfirmId] = useState('')
 
   const {
     setEditEventTitle,
@@ -33,17 +33,17 @@ export const EventCard = ({
     setEditEventImage,
     setEditEventDetails,
     setAddEvent,
-  } = cardEditData || {};
+  } = cardEditData || {}
 
   function onEdit(id) {
-    console.log(id);
-    setEditEventID(id);
-    setModalShow(true);
-    setAddEvent(false);
-    setEditEventTitle(eventTitle);
-    setEditEventTime(eventTime);
-    setEditEventImage(eventImage);
-    setEditEventDetails(eventDetails);
+    console.log(id)
+    setEditEventID(id)
+    setModalShow(true)
+    setAddEvent(false)
+    setEditEventTitle(eventTitle)
+    setEditEventTime(eventTime)
+    setEditEventImage(eventImage)
+    setEditEventDetails(eventDetails)
   }
 
   // this will notify all the users
@@ -52,97 +52,97 @@ export const EventCard = ({
       eventTitle,
       eventImage,
       eventDetails,
-      eventTime: moment(eventTime).format("lll"),
-    };
-    setConfirmId("");
-    setShowNotifyConfirm(false);
+      eventTime: moment(eventTime).format('lll'),
+    }
+    setConfirmId('')
+    setShowNotifyConfirm(false)
     try {
-      const authToken = localStorage.getItem("token");
+      const authToken = localStorage.getItem('token')
       let parseddata = await axios.post(`${Api}announceall`, mailData, {
         withCredentials: true,
         crossorigin: true,
         headers: { Authorization: `Bearer ${authToken}` },
-      });
+      })
       //console.log('MAIL SEND ........', parseddata)
       //console.log('MAIL DATA: ', mailData)
-      toast.success("Users Notified", {
-        theme: "dark",
-      });
+      toast.success('Users Notified', {
+        theme: 'dark',
+      })
     } catch {
-      toast.error("Failed To Notify Users");
+      toast.error('Failed To Notify Users')
     }
   }
 
   const signInFirst = () => {
     //console.log('Log In to your account to Register in this event')
-    toast.error("Sign In To Register To The Event", {
-      theme: "dark",
-      onClick: () => navigate("/signin"),
-    });
-  };
+    toast.error('Sign In To Register To The Event', {
+      theme: 'dark',
+      onClick: () => navigate('/signin'),
+    })
+  }
 
   // This will register the user for the event
   const registerToEvent = async (id) => {
     try {
       //console.log(`Events added to the user page ${id}`)
-      const authToken = localStorage.getItem("token");
+      const authToken = localStorage.getItem('token')
       //console.log('AuthToken :- ', authToken)
-      let parseddata = await axios.post(`${Api}registerevent/${id}`, "", {
+      let parseddata = await axios.post(`${Api}registerevent/${id}`, '', {
         withCredentials: true,
         crossorigin: true,
         headers: { Authorization: `Bearer ${authToken}` },
-      });
+      })
       //console.log('User data :- ', parseddata)
-      navigate("/dashboard");
-      toast.success("You have successfully registered", {
-        theme: "dark",
-      });
+      navigate('/dashboard')
+      toast.success('You have successfully registered', {
+        theme: 'dark',
+      })
     } catch {
-      toast.error("You are already Registered", {
-        theme: "dark",
-      });
+      toast.error('You are already Registered', {
+        theme: 'dark',
+      })
     }
-  };
+  }
 
   const onDelete = async (id) => {
     //console.log(`Deleting the event with id ${id}`)
-    const authToken = localStorage.getItem("token");
+    const authToken = localStorage.getItem('token')
     //console.log('AuthToken :- ', authToken)
     let { data } = await axios.delete(`${Api}delete/${id}`, {
       withCredentials: true,
       crossorigin: true,
       headers: { Authorization: `Bearer ${authToken}` },
-    });
+    })
     //console.log('User data :- ', data)
     if (data.success) {
-      window.location.reload();
+      window.location.reload()
     }
-  };
+  }
 
   function handleConfirmModal(id) {
-    setConfirmId(id);
-    setShowConfirm((prev) => !prev);
+    setConfirmId(id)
+    setShowConfirm((prev) => !prev)
   }
 
   function handleConfirmNotifyModal(id) {
-    setConfirmId(id);
-    setShowNotifyConfirm((prev) => !prev);
+    setConfirmId(id)
+    setShowNotifyConfirm((prev) => !prev)
   }
 
   return (
     <>
       <div className={`event-card`}>
-        <div className="event-card-img-container">
-          <img src={eventImage} alt="event-img" className="event-card-img" />
+        <div className='event-card-img-container'>
+          <img src={eventImage} alt='event-img' className='event-card-img' />
         </div>
-        <div className="card-text-details">
-          <p className="event-card-title">{eventTitle}</p>
-          <p className="event-card-date">
-            Date: <span>{eventTime.split("T")[0]}</span>
+        <div className='card-text-details'>
+          <p className='event-card-title'>{eventTitle}</p>
+          <p className='event-card-date'>
+            Date: <span>{eventTime.split('T')[0]}</span>
           </p>
           {tokenChecker ? (
             <>
-              <div className="event-card-desc">
+              <div className='event-card-desc'>
                 <ReactMarkdown>{eventDetails}</ReactMarkdown>
               </div>
             </>
@@ -150,7 +150,7 @@ export const EventCard = ({
             <></>
           )}
 
-          <div className="event-btn-wrapper">
+          <div className='event-btn-wrapper'>
             {/* {console.log("Decd Array -> ", decisionArray)} */}
             {tokenChecker ? (
               userRole >= 3 ? (
@@ -158,27 +158,20 @@ export const EventCard = ({
               ) : name ? (
                 <>
                   <button
-                    className="btn event-card-btn"
+                    className='btn event-card-btn'
                     onClick={() => registerToEvent(_id)}
                   >
                     Register
                   </button>
                 </>
               ) : (
-                <>
-                  <button
-                    className="btn event-card-btn"
-                    onClick={() => registerToEvent(_id)}
-                    disabled
-                  >
-                    Register
-                  </button>
-                </>
+                /* can't register in previous event */
+                ''
               )
             ) : (
               <button
-                className="btn event-card-btn"
-                style={{ display: "block", alignItems: "center" }}
+                className='btn event-card-btn'
+                style={{ display: 'block', alignItems: 'center' }}
                 onClick={signInFirst}
               >
                 Register
@@ -190,7 +183,7 @@ export const EventCard = ({
                 {userRole >= 2 ? (
                   <>
                     <button
-                      className="btn event-card-btn"
+                      className='btn event-card-btn'
                       onClick={() => onEdit(_id)}
                     >
                       Edit
@@ -200,7 +193,7 @@ export const EventCard = ({
                       name ? (
                         <>
                           <button
-                            className="btn event-card-btn"
+                            className='btn event-card-btn'
                             onClick={() => handleConfirmNotifyModal(_id)}
                           >
                             Notify
@@ -212,15 +205,15 @@ export const EventCard = ({
                               handleConfirmModal={handleConfirmNotifyModal}
                               eventId={_id}
                               func={notify}
-                              type="Notify"
-                              message="Are you sure you want to Notify the Users ? This will send email to all the registered users and this can not be undone, make sure the details are correct.."
+                              type='Notify'
+                              message='Are you sure you want to Notify the Users ? This will send email to all the registered users and this can not be undone, make sure the details are correct..'
                             />
                           )}
                         </>
                       ) : (
                         <>
                           <button
-                            className="btn event-card-btn"
+                            className='btn event-card-btn'
                             onClick={() => handleConfirmNotifyModal(_id)}
                             disabled
                           >
@@ -229,10 +222,10 @@ export const EventCard = ({
                         </>
                       )
                     ) : (
-                      ""
+                      ''
                     )}
                     <button
-                      className="btn event-card-btn"
+                      className='btn event-card-btn'
                       onClick={() => handleConfirmModal(_id)}
                     >
                       Delete
@@ -244,14 +237,14 @@ export const EventCard = ({
                         handleConfirmModal={handleConfirmModal}
                         eventId={_id}
                         func={onDelete}
-                        type="Delete"
-                        message="Are you sure you want to delete this Event? This will remove the Event
-                        and can not be undone"
+                        type='Delete'
+                        message='Are you sure you want to delete this Event? This will remove the Event
+                        and can not be undone'
                       />
                     )}
                   </>
                 ) : (
-                  ""
+                  ''
                 )}
               </>
             )}
@@ -259,5 +252,5 @@ export const EventCard = ({
         </div>
       </div>
     </>
-  );
-};
+  )
+}
