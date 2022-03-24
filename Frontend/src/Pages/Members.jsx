9944bdfc3693aa90c43244c6b4ffb5cafc1401dd
Soumya Banerjee <soumyabanerjee783@gmail.com>
2022-03-24 22:01:41 +0530

    teacher coordinators

--- Frontend/src/Pages/Members.jsx
+++ Frontend/src/Pages/Members.jsx
@@ -1,16 +1,16 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react'
 
-import './styles/Members.css';
-import Navbar from '../Components/Navbar';
-import MemberFilter from '../Components/MemberFilter';
-import MembersContainer from '../Components/MembersContainer';
-import UserContext from './Context/LoggedUserContext';
+import './styles/Members.css'
+import Navbar from '../Components/Navbar'
+import MemberFilter from '../Components/MemberFilter'
+import MembersContainer from '../Components/MembersContainer'
+import UserContext from './Context/LoggedUserContext'
 
 const Members = () => {
-  const [memberFilter, setMemberFilter] = useState('all');
+  const [memberFilter, setMemberFilter] = useState('all')
 
   function handleMemberFilter(selectedFilter) {
-    setMemberFilter(selectedFilter);
+    setMemberFilter(selectedFilter)
   }
 
   // const details = useContext(UserContext);
@@ -49,6 +49,39 @@ const Members = () => {
           </p>
         </header>
         <hr />
+        <h3 className='teacher-header'>Teacher Coordinators</h3>
+        <div className='teacher-wrapper'>
+          <div className='teacher-container'>
+            <div className='teacher-img'>
+              <img
+                src='https://res.cloudinary.com/sahebcloud/image/upload/v1647626528/125117693_357780665522683_8638435484497410468_n_osozlr.jpg'
+                alt='teacher'
+              />
+            </div>
+            <p className='teacher-name'>Sudip Kumar De</p>
+            <p className='teacher-designation'>Assistant proffesor, IT</p>
+          </div>
+          <div className='teacher-container'>
+            <div className='teacher-img'>
+              <img
+                src='https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80'
+                alt='teacher'
+              />
+            </div>
+            <p className='teacher-name'>Dr. Anup Mukhopadhyay</p>
+            <p className='teacher-designation'>HOD, IT</p>
+          </div>
+          <div className='teacher-container'>
+            <div className='teacher-img'>
+              <img
+                src='https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80'
+                alt='teacher'
+              />
+            </div>
+            <p className='teacher-name'>Biplab Mondal</p>
+            <p className='teacher-designation'>Assistant proffesor, IT</p>
+          </div>
+        </div>
         <MemberFilter
           memberFilter={memberFilter}
           handleMemberFilter={handleMemberFilter}
@@ -56,7 +89,7 @@ const Members = () => {
         <MembersContainer memberFilter={memberFilter} />
       </main>
     </>
-  );
-};
+  )
+}
 
-export default Members;
+export default Members
