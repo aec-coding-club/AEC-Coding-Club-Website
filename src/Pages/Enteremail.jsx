a605a8c5fc0606573d1e47de3ef049a54d6297cd
Soumya Banerjee <soumyabanerjee783@gmail.com>
2022-04-11 23:55:36 +0530

    Back Btn in Forgot Passwords Page

--- src/Pages/Enteremail.jsx
+++ src/Pages/Enteremail.jsx
@@ -1,32 +1,39 @@
-import React, { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { BiArrowBack } from "react-icons/bi";
-import EmailForme from "./Components/EmailForme";
-import forgotpassword from "../Assets/forgotpassword.svg";
+import React, { useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { IoIosArrowBack } from 'react-icons/io'
+import EmailForme from './Components/EmailForme'
+import forgotpassword from '../Assets/forgotpassword.svg'
 
 const Enteremail = () => {
-  let navigate = useNavigate();
+  let navigate = useNavigate()
   async function tokenCheker() {
-    const authToken = localStorage.getItem("token");
+    const authToken = localStorage.getItem('token')
     if (authToken) {
-      navigate("/");
+      navigate('/')
     }
   }
 
   useEffect(() => {
-    tokenCheker();
-  }, []);
+    tokenCheker()
+  }, [])
 
   return (
     <>
-      <div className="user-login ">
-        <div className="user-img">
-          <img alt="" src="https://res.cloudinary.com/sahebcloud/image/upload/v1649397165/forgot-password-animate_fyfjlr.svg"></img>
+      <div className='user-login forgot-password-wrapper'>
+        <div className='back-btn-login' onClick={() => navigate('/signin')}>
+          <IoIosArrowBack />
+          <p>Go Back</p>
+        </div>
+        <div className='user-img'>
+          <img
+            alt=''
+            src='https://res.cloudinary.com/sahebcloud/image/upload/v1649397165/forgot-password-animate_fyfjlr.svg'
+          ></img>
         </div>
         <EmailForme />
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Enteremail;
+export default Enteremail
