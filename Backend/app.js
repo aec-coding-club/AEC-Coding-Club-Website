1f033bd678816e305204476cabdd761fdf2c3009
Pranay <pranayguptamain@gmail.com>
2022-10-16 02:08:36 +0530

    ek 's' ki kimat tum kya jano

--- Backend/app.js
+++ Backend/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieparser());
 app.use(
   cors({
-    origin: "http://aeccc.targettechnology.in",
+    origin: "https://aeccc.targettechnology.in",
     credentials: true,
   })
 );
