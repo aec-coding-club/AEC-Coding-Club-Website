b7c489b6f0f49e0ec9c68d62506d905f1e1f9123
Pranay <pranayguptamain@gmail.com>
2022-10-15 20:25:28 +0530

    update members data

--- src/Components/MemberFilter.jsx
+++ src/Components/MemberFilter.jsx
@@ -1,27 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import './styles/MemberFilter.css';
+import "./styles/MemberFilter.css";
 
 const MemberFilter = ({ memberFilter, handleMemberFilter }) => {
   return (
-    <div className='member-filter'>
+    <div className="member-filter">
       <button
-        className={`btn all ${memberFilter === 'all' ? 'selected' : ''}`}
-        onClick={() => handleMemberFilter('all')}
-      >
+        className={`btn all ${memberFilter === "all" ? "selected" : ""}`}
+        onClick={() => handleMemberFilter("all")}>
         All
       </button>
       <button
-        className={`btn core ${memberFilter === 'core' ? 'selected' : ''}`}
-        onClick={() => handleMemberFilter('core')}
-      >
+        className={`btn core ${memberFilter === "core" ? "selected" : ""}`}
+        onClick={() => handleMemberFilter("core")}>
         Core Committee
       </button>
       <button
-        className={`btn sub ${memberFilter === 'sub' ? 'selected' : ''}`}
-        onClick={() => handleMemberFilter('sub')}
-      >
+        className={`btn sub ${memberFilter === "sub" ? "selected" : ""}`}
+        onClick={() => handleMemberFilter("sub")}>
         Sub Committee
+      </button>
+      <button
+        className={`btn sub ${memberFilter === "passout" ? "selected" : ""}`}
+        onClick={() => handleMemberFilter("passout")}>
+        Passout
       </button>
     </div>
   );
--- src/Components/MembersContainer.jsx
+++ src/Components/MembersContainer.jsx
@@ -1,47 +1,35 @@
-import React from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import './styles/MembersContainer.css';
-import { membersData } from '../data/members';
-import MemberCard from './MemberCard';
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import "./styles/MembersContainer.css";
+import { membersData } from "../data/members";
+import MemberCard from "./MemberCard";
 
 const MembersContainer = ({ memberFilter }) => {
   let allMembers;
-  if (memberFilter === 'all') {
+  if (memberFilter === "all") {
     allMembers = membersData.map((member) => {
-      return (
-        <MemberCard
-          member={member}
-          memberFilter={memberFilter}
-          key={uuidv4()}
-        />
-      );
+      return <MemberCard member={member} memberFilter={memberFilter} key={uuidv4()} />;
     });
-  } else if (memberFilter === 'core') {
+  } else if (memberFilter === "core") {
     allMembers = membersData
       .filter((member) => member.role === 2 || member.role === 3)
       .map((member) => {
-        return (
-          <MemberCard
-            member={member}
-            memberFilter={memberFilter}
-            key={uuidv4()}
-          />
-        );
+        return <MemberCard member={member} memberFilter={memberFilter} key={uuidv4()} />;
       });
-  } else if (memberFilter === 'sub') {
+  } else if (memberFilter === "sub") {
     allMembers = membersData
       .filter((member) => member.role === 1)
       .map((member) => {
-        return (
-          <MemberCard
-            member={member}
-            memberFilter={memberFilter}
-            key={uuidv4()}
-          />
-        );
+        return <MemberCard member={member} memberFilter={memberFilter} key={uuidv4()} />;
+      });
+  } else if (memberFilter === "passout") {
+    allMembers = membersData
+      .filter((member) => member.role === 4)
+      .map((member) => {
+        return <MemberCard member={member} memberFilter={memberFilter} key={uuidv4()} />;
       });
   }
-  return <div className='members-container'>{allMembers}</div>;
+  return <div className="members-container">{allMembers}</div>;
 };
 
 export default MembersContainer;
